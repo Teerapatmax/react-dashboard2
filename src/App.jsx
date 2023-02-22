ee66014b8cbfd42ea5f03280fec03c6384ecd255
Teerapat Prasongprasit <Teerapat13608@gmail.com>
2023-02-22 15:40:47 +0700

    Teerapat

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,59 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Chart from "react-apexcharts";
-
 
 function App() {
   const [petPrices, setPetPrice] = useState([])
-
   const [petChart, setPetChart] = useState({
     options: {
       chart: {
         id: "basic-bar"
       },
       xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+        categories: []
       }
     },
     series: [
       {
         name: "series-1",
-        data: [30, 40, 45, 50, 49, 60, 70, 91]
+        data: []
       }
     ]
   })
 
-
   useEffect(() => {
-    fetch('https://attractive-cyan-sheep.cyclic.app/pets_price')
+    fetch('https://spotless-sarong-newt.cyclic.app/pets_price')
     .then(res => res.json())
     .then(result => {
       console.log(result)
       setPetPrice(result)
     })
+
+    fetch('https://spotless-sarong-newt.cyclic.app/pets_price_chart')
+    .then(res => res.json())
+    .then(result => {
+      console.log(result)
+      setPetChart({
+        options: {
+          chart: {
+            id: "basic-bar"
+          },
+          xaxis: {
+            categories: result.petNames
+          }
+        },
+        series: [
+          {
+            name: "ราคา",
+            data: result.prices
+          }
+        ]
+      })
+    })
   }, [])
 
   return (
     <div>
-      <h1>Teerapat 6402802</h1>
+      <h1>Teerapat Prasongprasit 6402802</h1>
       <ul>
         {petPrices.map(pet => (
           <li key={pet.id}>
@@ -48,10 +67,8 @@ function App() {
         type='bar'
         width='500'
       />
-
     </div>
   )
-
 }
 
 export default App
